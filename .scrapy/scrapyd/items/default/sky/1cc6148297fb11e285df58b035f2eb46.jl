070503bcd1cf2ecbea8399ec84e32dda244dbc03
Mark Nemec <mrknmc@me.com>
2013-03-29 10:22:53 +0000

    this works

--- .scrapy/scrapyd/items/default/sky/1cc6148297fb11e285df58b035f2eb46.jl
+++
@@ -1,2 +0,0 @@
-[{"date": "03 November 2013", "dest": "BTS", "company": "Indirect flights", "price": "\u00a3156", "orig": "EDI"},
-{"date": "04 November 2013", "dest": "BTS", "company": "Indirect flights", "price": "\u00a3126", "orig": "EDI"}]
--- .scrapy/scrapyd/items/default/sky/2839e83597fc11e2abc258b035f2eb46.jl
+++
@@ -1,4 +0,0 @@
-[{"dest": "BTS", "company": "Norwegian", "price": "\u00a3160", "orig": "EDI"},
-{"dest": "BTS", "company": "Norwegian", "price": "\u00a3176", "orig": "EDI"},
-{"dest": "BTS", "company": "KLM + EL AL Israel Airlines + Smart Wings", "price": "\u00a31,509", "orig": "EDI"},
-{"dest": "BTS", "company": "Brussels Airlines + EL AL Israel Airlines + Smart Wings", "price": "\u00a31,518", "orig": "EDI"}]
